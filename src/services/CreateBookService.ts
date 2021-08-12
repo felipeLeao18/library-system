fb313716d63e2a3857bf7a40b038341a443ca0ec
felipeLeao18 <flpleao1807@gmail.com>
2021-08-12 07:03:49 -0300

    Added services  folder

---
+++ src/services/CreateBookService.ts
@@ -0,0 +1,34 @@
+import { getCustomRepository } from "typeorm";
+import { Book } from "../entities/Book";
+import { BooksRepositories } from "../repositories/BooksRepositories";
+
+interface IBookRequest {
+  title: string;
+  publisher: string;
+  picture: string;
+  authors: Array<string>;
+}
+
+class CreateBookService {
+  async execute({ title, publisher, picture, authors }: IBookRequest) {
+    const booksRepository = getCustomRepository(BooksRepositories);
+
+    if (!title) throw new Error("Books's name can't be empty");
+
+    const bookExists = await booksRepository.findOne({
+      title,
+    });
+    if (bookExists) throw new Error("Book already in the system");
+
+    const book = booksRepository.create({
+      title,
+      publisher,
+      picture,
+      authors,
+    });
+    await booksRepository.save(book);
+    return book;
+  }
+}
+
+export { CreateBookService };
