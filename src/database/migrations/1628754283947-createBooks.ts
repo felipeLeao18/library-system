a9df413e0696c20476d0ec2dfd051c44e617034e
felipeLeao18 <flpleao1807@gmail.com>
2021-08-12 05:36:54 -0300

    Updated package.json, Updated orm config - default path to any new migration, cli; Feat: createBooks migration

--- src/database/migrations/1628754283947-createBooks.ts
+++ src/database/migrations/1628754283947-createBooks.ts
@@ -1,42 +1,49 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class createBooks1628754283947 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "books",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "title",
+            type: "varchar",
+          },
+          {
+            name: "publisher",
+            type: "varchar",
+          },
+          {
+            name: "picture",
+            type: "varchar",
+          },
+          {
+            name: "authors",
+            isArray: true,
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.createTable(
-            new Table({
-                name:"books",
-                columns: [
-                    {
-                        name: "id",
-                        type: "uuid",
-                        isPrimary: true
-                    },
-                    {
-                        name: "title",
-                        type: "varchar"
-                    },
-                    {
-                        name: "publisher",
-                        type: "varchar",
-                    },
-                    {
-                        name: "picture",
-                        type: "varchar",
-                    },
-                       {
-                           name: "authors",
-                           isArray:true,
-                           type: "varchar"
-                       },
-
-                ]
-            })
-        )
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable("books");
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("books");
+  }
 }
