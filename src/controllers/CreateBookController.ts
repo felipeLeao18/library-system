3279614ac03666a69a5b99cded538114a07f1e67
felipeLeao18 <flpleao1807@gmail.com>
2021-08-12 07:03:26 -0300

    Added controllers folder

---
+++ src/controllers/CreateBookController.ts
@@ -0,0 +1,17 @@
+import { Request,response,Response } from "express";
+import { CreateBookService } from "../services/CreateBookService";
+
+
+class CreateBookController{
+    async handle(req: Request, res: Response){
+      const {title, publisher, picture, authors} = req.body;
+      
+      const createBookService = new CreateBookService();
+      const book = await createBookService.execute({title, publisher, picture, authors});
+      return res.json(book)
+    }
+
+
+}
+
+export {CreateBookController};
